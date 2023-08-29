fc46fa8e489d53802022750c4df25e1caf555711
Mathew Taylor <mathew.taylor2558@gmail.com>
2023-08-29 23:34:20 +0700

    added stuff to main.py

--- main.py
+++ main.py
@@ -1,3 +1,49 @@
 # Bank code stuff
-print("Hello from bank file")
+
+
+class FileFuncs:
+    @staticmethod
+    def ReadFile(filename: str):
+        with open(filename, "r") as f:
+            return f.read().splitlines()
+
+    @staticmethod
+    def AppendtoFile(filename: str, content: str):
+        with open(filename, "a") as f:
+            f.write(content + "\n")
+
+class Bank:
+    def __init__(self):
+        self.db = []
+
+    def load_bank_details(self):
+        # get bank details of every user
+        user_details = FileFuncs.ReadFile("Bank_details.txt")
+
+        # splitting each user info string into a list with proper data types
+        for i in range(len(user_details)):
+            # splitting the string into a list
+            user_details[i] = user_details[i].split()
+            
+            # making the user balance a float
+            user_details[i][-1] = float(user_details[i][-1])
+
+        self.db = user_details
+            
+            
+            
+    def add_user(self, username: str, password: str):
+        FileFuncs.AppendtoFile("Bank_details.txt", f"{username} {password} 0")
+
+
+
+
+BankObj = Bank()
+
+BankObj.add_user("Test124", "Test124")
+
+BankObj.load_bank_details()
+
+print(BankObj.db)
+
 input()
